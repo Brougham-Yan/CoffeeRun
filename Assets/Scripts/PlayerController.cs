00fd9216ec0544d73d6567f1d6becc9f0b26bb21
IAmAGorillaKing <zhanlai12@gmail.com>
2016-01-30 01:10:50 -0600

    Added stuff.
    - Added stage, with lighting
    - Added Player moving logics
    - Camera now follows the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float acceleration;
+    public float maxSpeed;
+	
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.freezeRotation = true;
+    }
+	void Update () {
+	    if(Input.GetKey(KeyCode.LeftArrow)) {
+            Rigidbody body = GetComponent<Rigidbody>();
+            float velX = body.velocity.x - (acceleration * Time.deltaTime);
+            velX = Mathf.Clamp(velX, -maxSpeed, maxSpeed);
+            body.velocity = new Vector3(velX, body.velocity.y, body.velocity.z);
+            
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            Rigidbody body = GetComponent<Rigidbody>();
+            float velX = body.velocity.x + (acceleration * Time.deltaTime);
+            velX = Mathf.Clamp(velX, -maxSpeed, maxSpeed);
+            body.velocity = new Vector3(velX, body.velocity.y, body.velocity.z);
+        }
+	}
+}
