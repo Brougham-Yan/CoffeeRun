a5129b6f0697d802884f81f70a90467a9c9261a3
Dan <danburns10@gmail.com>
2016-01-30 04:42:44 -0600

    +added a locational audio trigger that plays sound (sometimes) when you get close, with doppler effect +added a coffee temperature variable to the player that slowly decays over time -mostly fixed the jump momentum bug -you can now only jump while grounded -doubled the length of the test level... caused more weird lighting stuff to happen :/

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,38 +3,68 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public float coffeeTemperature;
+	public float temperatureDecay;
+
     public float acceleration;
     public float maxSpeed;
     bool jumping = false;
-	
+	public Vector3 velocity;
+	Rigidbody body;
+	float decayCooldown = 5f;//every 5 seconds, drop temperature by temperatureDecay
+	float decayTimer;
     void Start(){
-        Rigidbody body = GetComponent<Rigidbody>();
+		body = GetComponent<Rigidbody> ();
         body.freezeRotation = true;
+		decayTimer = decayCooldown;
     }
+		
 	void Update () {
-	    if(Input.GetKey(KeyCode.LeftArrow) && !jumping) {
-            Rigidbody body = GetComponent<Rigidbody>();
+		if (decayTimer > 0) {
+			decayTimer -= Time.deltaTime;
+		} else {
+			decayTimer = decayCooldown;
+			coffeeTemperature -= temperatureDecay;
+		}
+
+	    if(Input.GetKey(KeyCode.LeftArrow) ) {
+          //  Rigidbody body = GetComponent<Rigidbody>();
             float velX = body.velocity.x - (acceleration * Time.deltaTime);
             velX = Mathf.Clamp(velX, -maxSpeed, maxSpeed);
             body.velocity = new Vector3(velX, body.velocity.y, body.velocity.z);
-            
+			velocity.x = velX;
         }
-        else if (Input.GetKey(KeyCode.RightArrow) && !jumping) {
-            Rigidbody body = GetComponent<Rigidbody>();
+        else if (Input.GetKey(KeyCode.RightArrow) ) {
+           // Rigidbody body = GetComponent<Rigidbody>();
             float velX = body.velocity.x + (acceleration * Time.deltaTime);
             velX = Mathf.Clamp(velX, -maxSpeed, maxSpeed);
             body.velocity = new Vector3(velX, body.velocity.y, body.velocity.z);
+			velocity.x = velX;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space)) {
+		if(Input.GetKeyDown(KeyCode.Space) && !jumping) {
             jumping = true;
-            Rigidbody body = GetComponent<Rigidbody>();
+            //Rigidbody body = GetComponent<Rigidbody>();
             float velY = body.velocity.y + 5f;
             body.velocity = new Vector3(body.velocity.x, velY, body.velocity.z);
+			velocity.x = body.velocity.x; // save x velocity when you jump so you will keep it upon landing
         }
+
+		if (jumping) {
+			body.velocity = new Vector3(velocity.x, body.velocity.y, body.velocity.z);
+		}
+	}
+
+	void fixedUpdate()
+	{
+		if (jumping) {
+			body.velocity = new Vector3(velocity.x, body.velocity.y, body.velocity.z);
+		}
 	}
 
     void OnTriggerEnter(Collider collider) {
         jumping = false;
+		body.velocity = new Vector3(velocity.x, body.velocity.y, body.velocity.z); // when you land, keep x velocity; this mostly fixes the momentum loss (still a slight visual bug)
     }
+		
 }
---
+++ Assets/soundtrigger.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundtrigger : MonoBehaviour {
+
+	public AudioClip clip;
+	public float range;
+	public float playChance;
+	public float distance;
+	Transform target;
+	bool inRange;
+
+	// Use this for initialization
+	void Awake()
+	{
+		target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RangeCheck ();
+	}
+
+	void RangeCheck()
+	{
+		distance = Vector3.Distance (transform.position, target.transform.position);
+
+		if (distance < range && !inRange) {
+			playSound ();
+			inRange = true;
+		} else if (distance > range && inRange) {
+			inRange = false;
+		}
+	}
+
+	void playSound()
+	{
+		if (Random.value < playChance) {
+			AudioSource.PlayClipAtPoint (clip, transform.position);
+		}
+	}
+		
+}
